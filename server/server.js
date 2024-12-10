1866e410345b3ab8e19d747c100bac8f64aa4c29
jona-corbaxhi <jonacorbaxhi@gmail.com>
2024-12-09 22:34:44 -0500

    html file and server.py added

--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3000;
 
-app.use(express.static('public')); // Serve static files from the "public" folder
+// Serve static files
+app.use(express.static('public'));
+
+// Route for blocklyWork.html
+app.get('/blockly', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/blocklyWork.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
